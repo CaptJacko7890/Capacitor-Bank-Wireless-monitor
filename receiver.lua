2df2d6fef51b48eceb01e8192ecb54904b611014
CaptJacko7890 <Jacko7980@proton.me>
2025-05-29 14:32:09 +0100

    Update receiver.lua

--- receiver.lua
+++ receiver.lua
@@ -4,26 +4,7 @@ local term = require("term")
 local os = require("os")
 local gpu = component.gpu
 local tunnel = component.tunnel -- Linked card
-local serialization = require("serialization")
-
-gpu.setResolution(50, 15)
-
-local screenWidth, screenHeight = gpu.getResolution()
-term.clear()
-term.setCursorBlink(false)
-
-local lastReceiveTime = os.time()
-local connectionStatus = "Waiting for data..."
-local lastStatus = connectionStatus
-local latestData = nil
-local lastDrawnData = nil
-local lastStatusCheck = os.clock()
-local checkInterval = 1
-local timeout = 10
-
-local function centerText(text, y, color)
-  color = color or 0xFFFFFF
-  local x = math.floor((screenWidth - #text) / 2) + 1
+local serialization = require("se
   gpu.setForeground(color)
   term.setCursor(x, y)
   io.write(text)
